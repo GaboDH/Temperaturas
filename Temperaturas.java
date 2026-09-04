090f129fec39e23530783c0e452842726023c0b8
Gabriel Dávila <gabrieldavila1200@gmail.com>
2026-09-04 09:58:22 -0500

    Update Temperaturas.java

--- Temperaturas.java
+++ Temperaturas.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Temperaturas {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         int cantidad = 0;
@@ -21,39 +23,53 @@ public class Temperaturas {
         System.out.print("Temperatura: ");
         double temp = sc.nextDouble();
 
-     
         while (temp != 999) {
 
-
             if (temp < -50 || temp > 60) {
-                System.out.println("Dato invalido! Solo se acepta entre -50 y 60. El 80 es invalido.");
+
+                System.out.println("Dato invalido! Solo se acepta entre -50 y 60.");
+
             } else {
 
                 if (temp < 10) {
+
                     System.out.println(temp + " -> Frio");
                     frio++;
+
                 } else if (temp >= 10 && temp <= 24) {
+
                     System.out.println(temp + " -> Templado");
                     templado++;
+
                 } else if (temp >= 25 && temp <= 34) {
+
                     System.out.println(temp + " -> Calido");
                     calido++;
+
                 } else {
+
                     System.out.println(temp + " -> Muy calido");
                     muyCalido++;
                 }
 
-        
                 suma += temp;
                 cantidad++;
 
                 if (primero) {
+
                     mayor = temp;
                     menor = temp;
                     primero = false;
+
                 } else {
-                    if (temp > mayor) mayor = temp;
-                    if (temp < menor) menor = temp;
+
+                    if (temp > mayor) {
+                        mayor = temp;
+                    }
+
+                    if (temp < menor) {
+                        menor = temp;
+                    }
                 }
             }
 
@@ -61,23 +77,28 @@ public class Temperaturas {
             temp = sc.nextDouble();
         }
 
-
         System.out.println("\n--- RESULTADO FINAL ---");
+
         if (cantidad > 0) {
+
             double promedio = suma / cantidad;
+
             System.out.println("Cantidad de datos: " + cantidad);
             System.out.println("Mayor: " + mayor);
             System.out.println("Menor: " + menor);
             System.out.println("Promedio: " + promedio);
+
             System.out.println("Cantidad por categoria:");
             System.out.println("Frio: " + frio);
             System.out.println("Templado: " + templado);
             System.out.println("Calido: " + calido);
             System.out.println("Muy calido: " + muyCalido);
+
         } else {
+
             System.out.println("No se ingresaron datos validos.");
         }
-        
+
         sc.close();
     }
 }
